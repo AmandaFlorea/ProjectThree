7d56c4cf00baf254e611fb02d02c81eed0986880
AmandaFlorea <94856319+AmandaFlorea@users.noreply.github.com>
2021-11-24 20:45:07 -0500

    Update DemoThree.java
    github changes

--- src/three/DemoThree.java
+++ src/three/DemoThree.java
@@ -4,6 +4,8 @@ public class DemoThree {
 	public static void main(String[] args) {
 		
 		System.out.println("Push from project 3");
+		
+		System.out.println("Pull changes.... made from github");
 	}
 
 }
